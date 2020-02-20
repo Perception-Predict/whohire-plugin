422ab16c6f609c830dfbf484b63c8ab501602062
Manvir <mail@manvir.me>
2020-02-20 09:24:20 +0530

    Change how plugin get loaded

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 class HireWhoPlugin {
-    constructor(slug, container) {
-        if (!slug) throw Error("The business slug is needed for HireWho Plugin to load.");
-        if (!container) container = "hirewho-plugin";
-        this.slug = slug;
+    constructor(conf) {
+        if (!conf) throw Error("HireWhoPlugin conf not available");
+        if (!conf.slug) throw Error("The business slug is needed for HireWho Plugin to load.");
+        if (!conf.container) container = "hirewho-plugin";
+        this.slug = conf.slug;
         this.container = document.getElementById(container);
     }
 
     load() {
+        console.log("load called on HireWhoPlugin");
         this.container.innerHTML = "<p>This is loaded via plugin</p>";
     }
 }
+
+const e = new HireWhoPlugin(window.hirewhoConf);
+e.load();
